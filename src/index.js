39c0cf284d8110d94e531e8342fcd56614fd1de8
orimaromcom <ori.marom10@gmail.com>
2023-11-07 20:01:32 +0800

    add connect

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { goerli } from "wagmi/chains";
+import { infuraProvider } from "@wagmi/core/providers/infura";
 
+const INFURA_API = process.env.INFURA_API;
+const { publicClient, webSocketPublicClient } = configureChains(
+  [goerli],
+  [infuraProvider({ apiKey: INFURA_API })]
+);
+
+const config = createConfig({
+  autoConnect: true,
+
+  publicClient,
+  webSocketPublicClient,
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <WagmiConfig config={config}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </WagmiConfig>
   </React.StrictMode>
 );
