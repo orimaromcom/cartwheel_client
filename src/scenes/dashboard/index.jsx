e81417d88b2243cbe7668364b433c5a247584f1f
orimaromcom <ori.marom10@gmail.com>
2023-11-10 14:19:01 +0700

    improve

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 import Header from "../../components/Header";
 
 const Dashboard = ({ walletData, walletIsConnected, walletAddress }) => {
+  console.log(walletIsConnected);
   return (
     <Box m="20px">
       <Header title="DASHBOARD" subtitle="Customizable Dashboard" />
@@ -10,7 +11,7 @@ const Dashboard = ({ walletData, walletIsConnected, walletAddress }) => {
       {walletIsConnected ? (
         <div>
           Hello , This is your wallet address: {walletAddress}
-          <div>{walletData === "0" ? `Become Partner` : null}</div>
+          <div>{walletData === undefined ? `Become Partner` : null}</div>
         </div>
       ) : null}
     </Box>
